433dcc509cdade33125d916fdddc3e195c52bc79
Stefanie Serpa <smserpa@gmail.com>
2020-10-06 23:10:15 -0700

    moved code using inquirer package to app.js file. defined objects for each subclass of employee and pushed to team array

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,6 +14,88 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
+
+
+
+function teamData() {
+
+    const manager = [];
+    const engineer = [];
+    const intern = [];
+
+    const managerJSON = JSON.stringify(manager, null, 2);
+    const engineerJSON = JSON.stringify(engineer, null, 2);
+    const internJSON = JSON.stringify(intern, null, 2);
+
+    const team = [];
+    
+
+
+    return inquirer.prompt([
+        
+        {
+            name: "name",
+            type: "input",
+            message: "To add to your team please input the name of an employee:",
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "Enter the employee's ID number:"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Input employee's email:"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Select the employee's role:",
+            choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+            ]
+        },
+        {
+            name: "github",
+            type: "input",
+            message: "Enter GitHub username: ",
+            when: (answers) => answers.role === "Engineer",
+        },
+        {
+            name: "school",
+            type: "input",
+            message: "Enter school name: ",
+            when: (answers) => answers.role === "Intern",
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            message: "Enter office number: ",
+            when: (answers) => answers.role === "Manager",
+        },
+    ])
+    .then((answers) => {
+        if (answers.role === "Manager") {
+            manager.push(answers.name, answers.id, answers.email, answers.officeNumber);    
+        } else if (answers.role === "Engineer") {
+            engineer.push(answers.name, answers.id, answers.email, answers.github);
+        }else (answers.role === "Intern")
+            intern.push(answers.name, answers.id, answers.email, answers.school);
+    })
+    
+}
+
+
+teamData();
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -24,9 +107,6 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
